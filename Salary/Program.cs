82047610e735b172a1a4be8d205136fd208c6cee
Isabella Sanchis <isasanchis3@gmail.com>
2022-11-24 13:17:41 -0300

    Deploy

---
+++ Salary/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o salário ATUAL do colaborador:");
+
+            double employeeSalary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Salário atual do colaborador: R$ " + employeeSalary);
+
+            double readjust = employeeSalary * 1.1;
+
+            Console.WriteLine("Novo salário, com reajuste: R$ " + readjust);
+
+        }
+    }
+}
